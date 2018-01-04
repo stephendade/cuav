c351ab53c7bc7bb993231407c26d9ffbd0ce91b3
Stephen Dade <stephen_dade@hotmail.com>
2018-01-04 22:58:36 +1100

    Ras Pi capture - added mkdir

--- capturescripts/RasPi/start_rpi_capture.sh
+++ capturescripts/RasPi/start_rpi_capture.sh
@@ -9,7 +9,9 @@ export TZ=GMT
 CAPTURE_DIR=~/images_captured
 DATETIME_DIR=$(date +"%Y%m%d_%H-%M-%S")
 
-# start rpi capture. Images stored in PNG_DIR
+mkdir -p ${CAPTURE_DIR}/${DATETIME_DIR}
+
+# start rpi capture. Images stored in ${CAPTURE_DIR}/${DATETIME_DIR}
 screen -L -d -m -S rpi_capture -s /bin/bash ./cuavraw -o ${CAPTURE_DIR}/${DATETIME_DIR}
 
 # start MAVProxy logging
